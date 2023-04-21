d310a296e31e01de96ba53760d5f7b191bdda27e
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-04-21 22:00:01 +0200

    fix(mouse): implement translation selection

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ main_control_tab.add(test_tab, text="Test")
 main_control_tab.add(config_tab, text="Config")
 main_control_tab.pack(expand=1, fill="both")
 
-gui.run.Tab(run_tab)
-gui.test.Tab(test_tab)
-gui.config.Tab(config_tab)
+run = gui.run.Tab(run_tab)
+test = gui.test.Tab(test_tab)
+config = gui.config.Tab(config_tab)
 
 
 # Credits
@@ -69,8 +69,6 @@ app_running = True
 active = False
 mouse_x = 0
 mouse_y = 0
-mechwarrior5v1 = False
-mechwarrior5v2 = False
 autocenter = False
 debugging = False
 max_refresh_rate = 165
@@ -82,6 +80,11 @@ deadzone = 10
 
 # Main loop
 while app_running:
+    # Get configuation
+    translation_method = config.get_translation_method()
+
+
+    # Handle PyGame events
     for event in pygame.event.get():
         # Handle hotplugging
         if event.type == pygame.JOYDEVICEADDED:
@@ -114,14 +117,21 @@ while app_running:
         if joy.get_guid() == "030000001d2300000002000000000000": # Gladiator NXT
             if active:
                 # set mouse position
-                if mechwarrior5v1: # absolute mouse movement
+                if translation_method == 1: # default
+                    x_axis_value = int(joy.get_axis(0) * 5000)
+                    y_axis_value = int(joy.get_axis(1) * 5000)
+                    mouse_x_pos = mouse_x + x_axis_value
+                    mouse_y_pos = mouse_y - y_axis_value
+                    pydirectinput.moveTo(mouse_x_pos, mouse_y_pos, _pause=False)
+                
+                elif translation_method == 2: # absolute mouse movement
                     x_axis_value = int(joy.get_axis(0) * screen_x_center)
                     y_axis_value = int(joy.get_axis(1) * screen_y_center)
                     mouse_x_pos = screen_x_center + x_axis_value
                     mouse_y_pos = screen_y_center - y_axis_value
                     pydirectinput.moveTo(mouse_x_pos, mouse_y_pos, _pause=False)
 
-                elif mechwarrior5v2: # relavitve mouse movement
+                elif translation_method == 3: # relavitve mouse movement
                     x_axis_value = int(joy.get_axis(0) * joystick_resolution)
                     y_axis_value = int(joy.get_axis(1) * joystick_resolution)
                     mouse_Dx = x_axis_value - last_mouse_x
@@ -130,13 +140,6 @@ while app_running:
                     last_mouse_y = y_axis_value
                     pydirectinput.moveRel(mouse_Dx,mouse_Dy, _pause=False, relative=True)
 
-                else: # default
-                    x_axis_value = int(joy.get_axis(0) * 5000)
-                    y_axis_value = int(joy.get_axis(1) * 5000)
-                    mouse_x_pos = mouse_x + x_axis_value
-                    mouse_y_pos = mouse_y - y_axis_value
-                    pydirectinput.moveTo(mouse_x_pos, mouse_y_pos, _pause=False)
-                
 
                 # center torso if joystick is in deadzone
                 if autocenter:
@@ -148,10 +151,10 @@ while app_running:
 
 
                 if debugging:
-                    if mechwarrior5v2:
-                        print(f"dX: {mouse_Dx} \tdY: {mouse_Dy}")
+                    if translation_method == 3:
+                        print(f"Mouse: \tdX: {mouse_Dx} \tdY: {mouse_Dy}")
                     else:
-                        print(f"X: {x_axis_value} \tY: {y_axis_value}")
+                        print(f"Mouse: \tX: {x_axis_value} \tY: {y_axis_value}")
                 
 
                 # extra functionality for buttons
