b0d9eb0fc4b4edd42b0b8856c9cbd61f0ce9b78c
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-04-21 23:13:00 +0200

    fix(GUI - Run): Add run state indicator

--- gui/run/Tab.py
+++ gui/run/Tab.py
@@ -6,6 +6,7 @@ class Tab():
     def __init__(self, tab):
         '''This is the main tab for running the application'''
         self.armed = tk.BooleanVar(value=False)
+        self.running = False
         
         self.tab = tab
         self.row_1 = ttk.Frame(self.tab)
@@ -20,15 +21,46 @@ class Tab():
         self.arm_button = ttk.Checkbutton(
             self.row_1,
             text="Arm",
-            variable=self.armed
+            variable=self.armed,
+            command=self.update_status
         )
         self.arm_button.pack(side="top", fill="x", padx=10)
 
+        self.status_label = ttk.Label(
+            self.row_1,
+            text="Disarmed",
+            foreground="white",
+            background="green",
+            font=("Helvetica", 16),
+            anchor="center",
+            padding=20,
+            border=2,
+            borderwidth=2
+        )
+        self.status_label.pack(side="bottom", fill="x", padx=10, pady=6)
+
     def get_armed(self):
         return self.armed.get()
-    
+
+
     def enable_arming(self):
         self.arm_button.config(state="normal")
-        
+
+
     def disable_arming(self):
         self.arm_button.config(state="disabled")
+
+
+    def update_status(self):
+        if self.armed.get() and not self.running:
+            self.status_label.config(
+                text="Armed",
+                foreground="white",
+                background="orange"
+            )
+        else:
+            self.status_label.config(
+                text="Disarmed",
+                foreground="white",
+                background="green"
+            )
