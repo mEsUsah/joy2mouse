2479783f14fa99c2629713f1d2e76dc1945c47b7
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-04-21 22:50:39 +0200

    fix(GUI - Run): Add checkbox to arm program

--- gui/run/Tab.py
+++ gui/run/Tab.py
@@ -5,11 +5,24 @@ import gui
 class Tab():
     def __init__(self, tab):
         '''This is the main tab for running the application'''
-        elementaryTabContent = ttk.Frame(tab)
-        elementaryTabContent.pack(side="top", expand=1, fill="both")
+        self.armed = tk.BooleanVar(value=False)
+        
+        self.tab = tab
+        self.row_1 = ttk.Frame(self.tab)
+        self.row_1.pack(side="top", expand=1, fill="both")
 
-        tab1_label = ttk.Label(
-            elementaryTabContent, 
-            text="This is the main tab"
+        self.row_1_label = ttk.Label(
+            self.row_1, 
+            text="Arm to run, then activate to run"
         )
-        tab1_label.pack(side="top", fill="x", padx=10, pady=10)
+        self.row_1_label.pack(side="top", fill="x", padx=10, pady=6)
+
+        self.arm_button = ttk.Checkbutton(
+            self.row_1,
+            text="Arm",
+            variable=self.armed
+        )
+        self.arm_button.pack(side="top", fill="x", padx=10)
+
+    def get_armed(self):
+        return self.armed.get()
