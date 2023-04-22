c0bec08478d775f75d9318319f808bedf6c18c2c
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-04-22 17:55:54 +0200

    fix(GUI - Config): add axis selection

--- gui/config/Tab.py
+++ gui/config/Tab.py
@@ -10,9 +10,13 @@ class Tab():
         self.translation_method = tk.IntVar(value=1)
         self.joystick_resolution = tk.IntVar(value=16)
         self.autocenter = tk.BooleanVar(value=False)
+
         self.joysticks = {}
         self.joystick_selected = tk.StringVar(value="None")
+        self.joystick_x_selected = tk.StringVar(value="None")
+        self.joystick_y_selected = tk.StringVar(value="None")
         
+
         self.row_1 = ttk.Frame(self.tab)
         self.row_1.pack(side="top", expand=1, fill="both")
 
@@ -76,11 +80,20 @@ class Tab():
             font="TkDefaultFont 10 bold"
         )
         self.joystick_select_label.pack(side="top", fill="x", padx=10, pady=6)
+        
+        # X axis selection
+        self.row_5 = ttk.Frame(self.tab)
+        self.row_5.pack(side="top", expand=1, fill="both", pady=6)
+        
+        # X axis selection
+        self.row_6 = ttk.Frame(self.tab)
+        self.row_6.pack(side="top", expand=1, fill="both", pady=6)
 
 
     def update_device_list(self, joysticks):
         self.joysticks = joysticks
         self.update_joystick_combobox()
+        self.update_axis_selection()
 
 
     def update_joystick_combobox(self):
@@ -99,9 +112,66 @@ class Tab():
             self.row_4,
             values=joystick_list_values,
             textvariable=self.joystick_selected,
-            state="readonly"
+            state="readonly",
         )
+        self.joystick_list.bind('<<ComboboxSelected>>', self.update_axis_selection)    
         self.joystick_list.pack(side="top", fill="x", padx=10)
+
+
+    def update_axis_selection(self,event=None):
+        # Destroy axis selection if it exists and deselect axis
+        try:
+            self.x_axis_label.destroy()
+            self.x_axis_list.destroy()
+            self.y_axis_label.destroy()
+            self.y_axis_list.destroy()
+            self.joystick_x_selected.set("None")
+            self.joystick_y_selected.set("None")
+        except:
+            pass
+
+        if self.joystick_selected.get() != "None":
+            for device in self.joysticks.values():
+                if device.get_name() == self.joystick_selected.get():
+                    axis_list = [x+1 for x in range(device.get_numaxes())]
+
+                    # X axis selection
+                    self.x_axis_label = ttk.Label(
+                        self.row_5,
+                        text="X axis:",
+                    )
+                    self.x_axis_label.pack(side="left", padx=10)
+                    self.x_axis_list = ttk.Combobox(
+                        self.row_5,
+                        values=axis_list,
+                        state="readonly",
+                        textvariable=self.joystick_x_selected,
+                    )
+                    self.x_axis_list.bind('<<ComboboxSelected>>', self.update_joystick_x_axis)    
+                    self.x_axis_list.pack(side="left", padx=10)
+                    
+                    # Y axis selection
+                    self.y_axis_label = ttk.Label(
+                        self.row_6,
+                        text="Y axis:",
+                    )
+                    self.y_axis_label.pack(side="left", padx=10)
+                    self.y_axis_list = ttk.Combobox(
+                        self.row_6,
+                        values=axis_list,
+                        state="readonly",
+                        textvariable=self.joystick_y_selected,
+                    )
+                    self.y_axis_list.bind('<<ComboboxSelected>>', self.update_joystick_y_axis)  
+                    self.y_axis_list.pack(side="left", padx=10)
+                    
+                    
+    def update_joystick_x_axis(self, event=None):
+        print(self.joystick_x_selected.get())
+
+
+    def update_joystick_y_axis(self, event=None):
+        print(self.joystick_y_selected.get())
 
 
     def get_translation_method(self):
