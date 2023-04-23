f5179502835da8e377e2859adb51042cec8a4c1b
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2023-04-23 16:22:35 +0200

    fix(files) add open file functionality

--- utils/files.py
+++ utils/files.py
@@ -4,3 +4,9 @@ def gui_save_as_path():
     return filedialog.asksaveasfilename(
             filetypes=(("INI", "*.ini"), ("All files", "*.*")),
             defaultextension=".ini")
+
+
+def gui_open_path():
+    return filedialog.askopenfilename(
+            filetypes=(("INI", "*.ini"), ("All files", "*.*")),
+            defaultextension=".ini")
