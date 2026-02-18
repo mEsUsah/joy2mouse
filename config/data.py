6e27837532ee59d13333769b9bc71be980c0b963
mEsUsah <stanley@haxor.no>
2026-02-18 12:29:37 +0000

    refactor(config-data): clean up

--- config/data.py
+++ config/data.py
@@ -1,6 +1,56 @@
 import configparser
 
 
+configModel = {
+    'screen_x_center': 0,
+    'screen_y_center': 0,
+    'current_config_file': 'default.ini',
+    'current_config_default': True,
+    'armed': False,
+    'active': False,
+    'activated': False,
+    'debugging': False,
+    'activate_button_pressed': False,
+    'deactivate_button_pressed': False,
+    'mouse_x': 0,
+    'mouse_y': 0,
+    'last_mouse_x': 0,
+    'last_mouse_y': 0,
+    'max_refresh_rate': 165,
+
+    # Joystick
+    'translation_method': 1,
+    'joystick_resolution': 16,
+    'selected_joystick': None,
+    'selected_joystick_uuid': None,
+
+    'joystick_x_axis': None,
+    'joystick_y_axis': None,
+    'joystick_x_inverted': False,
+    'joystick_y_inverted': False,
+
+    'mouse_left_button': None,
+    'mouse_right_button': None,
+    'mouse_left_inverted': False,
+    'mouse_right_inverted': False,
+
+    'deadzone': 10,
+    'autocenter': False,
+    'autocenter_key': None,
+
+    # Buttonbox
+    'activation_method': 1,
+    'selected_buttonbox': None,
+    'selected_buttonbox_uuid': None,
+    'activation_button': None,
+    'deactivation_button': None,
+    'activation_button_inverted': False,
+    'deactivation_button_inverted': False,
+}
+
+joysticks = {}
+
+
 def get_current_config():
     current_config = configparser.ConfigParser()
     current_config['JOYSTICK'] = {
@@ -33,66 +83,8 @@ def get_current_config():
 def joystick_config_ready():
     # Only allow arming if at least one axis and activation button is set
     axis_selected = configModel['joystick_x_axis'] is not None or configModel['joystick_y_axis'] is not None
-    activation_button_selected = configModel['activation_button'] is not None
+    activation_ready = axis_selected and configModel['activation_button'] is not None
     # If activation method is 3 (on/off button), require both activation and deactivation button
-    if int(configModel.get('activation_method', 1)) == 3:
-        deactivation_button_selected = configModel['deactivation_button'] is not None
-        if axis_selected and activation_button_selected and deactivation_button_selected:
-            return True
-        else:
-            return False
-    else:
-        if axis_selected and activation_button_selected:
-            return True
-        else:
-            return False
-
-configModel = {
-    'screen_x_center': 0,
-    'screen_y_center': 0,
-    'current_config_file': 'default.ini',
-    'current_config_default': True,
-    'armed': False,
-    'active': False,
-    'activated': False,
-    'debugging': False,
-    'activate_button_pressed': False,
-    'deactivate_button_pressed': False,
-    'mouse_x': 0,
-    'mouse_y': 0,
-    'last_mouse_x': 0,
-    'last_mouse_y': 0,
-    'max_refresh_rate': 165,
-    'joystick_resolution': 0,
-
-    # Joystick
-    'translation_method': 1,
-    'joystick_resolution': 16,
-    'selected_joystick': None,
-    'selected_joystick_uuid': None,
-
-    'joystick_x_axis': None,
-    'joystick_y_axis': None,
-    'joystick_x_inverted': False,
-    'joystick_y_inverted': False,
-
-    'mouse_left_button': None,
-    'mouse_right_button': None,
-    'mouse_left_inverted': False,
-    'mouse_right_inverted': False,
-            
-    'deadzone': 10,
-    'autocenter': False,
-    'autocenter_key': None,
-
-    # Buttonbox
-    'activation_method': 1,
-    'selected_buttonbox': None,
-    'selected_buttonbox_uuid': None,
-    'activation_button': None,
-    'deactivation_button': None,
-    'activation_button_inverted': False,
-    'deactivation_button_inverted': False,
-}
-
-joysticks = {}
+    if int(configModel['activation_method']) == 3:
+        return activation_ready and configModel['deactivation_button'] is not None
+    return activation_ready
