92de03913c8b913eb6e4aeea5c0321afa5758a88
mEsUsah <stanley@haxor.no>
2026-02-18 12:31:49 +0000

    refactor(config-save): clean up

--- config/save.py
+++ config/save.py
@@ -6,7 +6,7 @@ def save_config():
     if config.data.configModel['current_config_file'] == 'default.ini':
         file_path = utils.files.gui_save_as_path()
         if not file_path:
-            return # User canceled the save dialog
+            return  # User canceled the save dialog
         config.data.configModel['current_config_file'] = file_path
 
     current_config = config.data.get_current_config()
@@ -17,9 +17,9 @@ def save_config():
 def save_config_as():
     file_path = utils.files.gui_save_as_path()
     if not file_path:
-        return # User canceled the save dialog
+        return  # User canceled the save dialog
     config.data.configModel['current_config_file'] = file_path
 
     current_config = config.data.get_current_config()
-    with open(config.data.configModel['current_config_file'], 'w') as configfile:
+    with open(file_path, 'w') as configfile:
         current_config.write(configfile)
