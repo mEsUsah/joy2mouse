a6d48c6e361f300a39cd82a0f5905a7d368cf7ba
mEsUsah <stanley@haxor.no>
2026-02-18 12:30:46 +0000

    refactor(config-load): clean up

--- config/load.py
+++ config/load.py
@@ -2,7 +2,6 @@ import config
 import utils
 import configparser
 import os
-import tkinter as tk
 
 
 def load_config_file():
@@ -10,51 +9,44 @@ def load_config_file():
 
     file_path = utils.files.gui_open_path()
     if not file_path:
-        return # User canceled the open dialog
+        return  # User canceled the open dialog
     config.data.current_config_file = file_path
 
     load_config()
 
 
 def load_config():
-    if config.data.configModel['current_config_default']:
-        config.data.configModel['current_config_file'] = os.path.join(os.getcwd(), 'default.ini')
-    
-    config.data.configModel['current_config'] = configparser.ConfigParser()
-    config.data.configModel['current_config'].read(config.data.configModel['current_config_file'])
+    model = config.data.configModel
 
-    # try:
+    if model['current_config_default']:
+        model['current_config_file'] = os.path.join(os.getcwd(), 'default.ini')
+
+    cfg = configparser.ConfigParser()
+    cfg.read(model['current_config_file'])
+
     # Joystick
-    config.data.configModel['translation_method'] = config.data.configModel['current_config'].getint('JOYSTICK', 'translation_method')
-    config.data.configModel['joystick_resolution'] = config.data.configModel['current_config'].getint('JOYSTICK', 'joystick_resolution')
-    config.data.configModel['joystick_selected'] = config.data.configModel['current_config'].get('JOYSTICK', 'selected_joystick')
+    model['translation_method'] = cfg.getint('JOYSTICK', 'translation_method')
+    model['joystick_resolution'] = cfg.getint('JOYSTICK', 'joystick_resolution')
+    model['joystick_selected'] = cfg.get('JOYSTICK', 'selected_joystick')
 
-    config.data.configModel['joystick_x_axis'] = config.data.configModel['current_config'].get('JOYSTICK', 'selected_x_axis')
-    config.data.configModel['joystick_y_axis'] = config.data.configModel['current_config'].get('JOYSTICK', 'selected_y_axis')
-    config.data.configModel['joystick_x_inverted'] = config.data.configModel['current_config'].getboolean('JOYSTICK', 'joystick_x_inverted')
-    config.data.configModel['joystick_y_inverted'] = config.data.configModel['current_config'].getboolean('JOYSTICK', 'joystick_y_inverted')
+    model['joystick_x_axis'] = cfg.get('JOYSTICK', 'selected_x_axis')
+    model['joystick_y_axis'] = cfg.get('JOYSTICK', 'selected_y_axis')
+    model['joystick_x_inverted'] = cfg.getboolean('JOYSTICK', 'joystick_x_inverted')
+    model['joystick_y_inverted'] = cfg.getboolean('JOYSTICK', 'joystick_y_inverted')
 
-    config.data.configModel['mouse_left_button'] = config.data.configModel['current_config'].getint('JOYSTICK', 'mouse_left_button')
-    config.data.configModel['mouse_right_button'] = config.data.configModel['current_config'].getint('JOYSTICK', 'mouse_right_button')
-    config.data.configModel['mouse_left_inverted'] = config.data.configModel['current_config'].getboolean('JOYSTICK', 'mouse_left_inverted')
-    config.data.configModel['mouse_right_inverted'] = config.data.configModel['current_config'].getboolean('JOYSTICK', 'mouse_right_inverted')
-    
-    config.data.configModel['autocenter'] = config.data.configModel['current_config'].getboolean('JOYSTICK', 'autocenter')
-    config.data.configModel['autocenter_key'] = config.data.configModel['current_config'].get('JOYSTICK', 'autocenter_key')
-    config.data.configModel['deadzone'] = config.data.configModel['current_config'].getint('JOYSTICK', 'deadzone')
+    model['mouse_left_button'] = cfg.getint('JOYSTICK', 'mouse_left_button')
+    model['mouse_right_button'] = cfg.getint('JOYSTICK', 'mouse_right_button')
+    model['mouse_left_inverted'] = cfg.getboolean('JOYSTICK', 'mouse_left_inverted')
+    model['mouse_right_inverted'] = cfg.getboolean('JOYSTICK', 'mouse_right_inverted')
 
+    model['autocenter'] = cfg.getboolean('JOYSTICK', 'autocenter')
+    model['autocenter_key'] = cfg.get('JOYSTICK', 'autocenter_key')
+    model['deadzone'] = cfg.getint('JOYSTICK', 'deadzone')
 
     # Buttonbox
-    config.data.configModel['activation_method'] = config.data.configModel['current_config'].getint('BUTTONBOX', 'selected_activation_method')
-    config.data.configModel['buttonbox_selected'] = config.data.configModel['current_config'].get('BUTTONBOX', 'selected_buttonbox')
-    config.data.configModel['activation_button'] = config.data.configModel['current_config'].getint('BUTTONBOX', 'activation_button')
-    config.data.configModel['deactivation_button'] = config.data.configModel['current_config'].getint('BUTTONBOX', 'deactivation_button')
-    config.data.configModel['activation_button_inverted'] = config.data.configModel['current_config'].getboolean('BUTTONBOX', 'activation_button_inverted')
-    config.data.configModel['deactivation_button_inverted'] = config.data.configModel['current_config'].getboolean('BUTTONBOX', 'deactivation_button_inverted')
-
-    # Config filename
-    # window.title(os.path.basename(config.data.current_config_file) + " - " + utils.releases.APP_NAME)
-    # except:
-    #     tk.messagebox.showerror("Open error", "Broken config file")
-
-
+    model['activation_method'] = cfg.getint('BUTTONBOX', 'selected_activation_method')
+    model['buttonbox_selected'] = cfg.get('BUTTONBOX', 'selected_buttonbox')
+    model['activation_button'] = cfg.getint('BUTTONBOX', 'activation_button')
+    model['deactivation_button'] = cfg.getint('BUTTONBOX', 'deactivation_button')
+    model['activation_button_inverted'] = cfg.getboolean('BUTTONBOX', 'activation_button_inverted')
+    model['deactivation_button_inverted'] = cfg.getboolean('BUTTONBOX', 'deactivation_button_inverted')
